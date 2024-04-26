45184161a4178ce78718cd75b8b49686891aaffd
sakshi pareek <sakshipareek505@gmail.com>
2024-04-26 10:17:31 +0530

    add web

--- src/components/HeroHeading.jsx
+++ src/components/HeroHeading.jsx
@@ -1,7 +1,7 @@
 export default function HeroHeading(props) {
   return (
     <>
-      <div className="container lg:max-w-[1202px] mx-auto px-3 xl:my-40 lg:my-24 md:my-20 my-12 xl:pt-5">
+      <div className="container lg:max-w-[1202px] mx-auto px-3 md:my-20 my-12 xl:pt-5">
         <h1 className="lg:text-textxxl md:text-textxl text-textlg text-center text-black02 font-semibold leading-[125%]">
           {props.heading}
         </h1>
